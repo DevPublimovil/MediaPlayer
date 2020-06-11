3f2287a8a70d9c38f7aae27ecd80d568ff8d2da9
DevPublimovil <60439610+DevPublimovil@users.noreply.github.com>
2020-06-11 15:42:24 -0600

    Validacion de memoria vacia

--- app/src/main/java/com/example/videotest/MainActivity.java
+++ app/src/main/java/com/example/videotest/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.videotest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Notification;
 import android.app.usage.ExternalStorageStats;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -27,34 +31,37 @@ public class MainActivity extends AppCompatActivity {
     VideoView videoView;
     ArrayList<File> videos;
     int video;
+
+    final int REQUEST_ACCESS_FINE=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        pantallaFull();
         setContentView(R.layout.activity_main);
-
         videoView=(VideoView)findViewById(R.id.videoView);
-
-        videos=readSongs(Environment.getExternalStorageDirectory());
-        Toast.makeText(this, videos.get(0).toString(), Toast.LENGTH_SHORT).show();
-        //INICIA EL REPRODUCTOR
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_ACCESS_FINE);
+        }else {
+            pantallaFull();
+            playVideo();
             reproducir();
+        }
+
             videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mediaPlayer) {
                     pantallaFull();
                     ++video;
-                    if(video>videos.size()-1){
-                        video=0;
+                    if (video > videos.size() - 1) {
+                        video = 0;
                     }
-                    String x=videos.get(video).toString();
+                    String x = videos.get(video).toString();
                     videoView.setVideoPath(x);
                     videoView.start();
-                    Toast.makeText(MainActivity.this, "termino "+videos.get(video), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "termino " + videos.get(video), Toast.LENGTH_SHORT).show();
                 }
             });
-            //Toast.makeText(this, songs.get(j).toString(),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, songs.get(j).toString(),Toast.LENGTH_SHORT).show();
     }
     ArrayList<File> readSongs(File root){
         ArrayList<File> arrayList = new ArrayList<File>();
@@ -71,11 +78,37 @@ public class MainActivity extends AppCompatActivity {
         return arrayList;
     }
 
+    public void showAlertDialogButtonClicked(){
+
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Memoria Vacía");
+        builder.setMessage("No se encontraron archivos mp4.");
+
+        // add a button
+        builder.setPositiveButton("SALIR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do something like...
+                finish();
+            }
+        });
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     public void reproducir(){
         videoView.setMediaController(new MediaController(this));
-        String x=videos.get(0).toString();
-        videoView.setVideoPath(x);
-        videoView.start();
+        if(videos.size()==0){
+            showAlertDialogButtonClicked();
+            //Toast.makeText(this, "No se encontraron videos mp4", Toast.LENGTH_SHORT).show();
+        }else{
+            String x=videos.get(0).toString();
+            videoView.setVideoPath(x);
+            videoView.start();
+        }
+
     }
     public void pantallaFull(){
         View decorView =getWindow().getDecorView();
@@ -89,5 +122,25 @@ public class MainActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_ACCESS_FINE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permitir", Toast.LENGTH_SHORT).show();
+                pantallaFull();
+                playVideo();
+                //Toast.makeText(this, videos.get(0).toString(), Toast.LENGTH_SHORT).show();
+                //INICIA EL REPRODUCTOR
+                reproducir();
+        }else {
+                Toast.makeText(this, "Cerrado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void playVideo(){
+        videos = readSongs(Environment.getExternalStorageDirectory());
     }
 }
